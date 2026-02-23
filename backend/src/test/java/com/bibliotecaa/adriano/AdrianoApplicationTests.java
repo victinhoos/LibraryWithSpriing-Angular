ad8013c025d2c7ea672eace761a847d42a10b576
Victor <victoreliasdasilva.profissional@outlook.com>
2026-02-23 16:05:32 -0300

    cricacao dos models (Livro, Editora e Autor)

---
+++ backend/src/test/java/com/bibliotecaa/adriano/AdrianoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bibliotecaa.adriano;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdrianoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
